2b4feabe0b7f9e71f40d547ee823c0ea52d53f36
konkz <konki@koks-nutten.de>
2025-12-26 15:33:23 +0100

    fix: use async_register_static_paths for HA compatibility

--- custom_components/faber_itc/__init__.py
+++ custom_components/faber_itc/__init__.py
@@ -6,11 +6,13 @@ from .client import FaberClient
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Faber ITC from a config entry."""
     # Registriert den Ordner 'branding' unter dem URL-Pfad '/faber_itc_static'
-    hass.http.register_static_path(
-        "/faber_itc_static",
-        hass.config.path("custom_components/faber_itc/branding"),
-        cache_headers=True
-    )
+    await hass.http.async_register_static_paths([
+        hass.http.StaticPathConfig(
+            "/faber_itc_static",
+            hass.config.path("custom_components/faber_itc/branding"),
+            True
+        )
+    ])
     
     host = entry.data[CONF_HOST]
     client = FaberClient(host, DEFAULT_PORT)
