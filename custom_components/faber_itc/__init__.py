0f6e73d05c538302adaf367d0559cb5aa103e6e1
konkz <konki@koks-nutten.de>
2025-12-29 00:41:34 +0100

    chore: cleanup __init__.py logs and comments

--- custom_components/faber_itc/__init__.py
+++ custom_components/faber_itc/__init__.py
@@ -10,9 +10,9 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Faber ITC from a config entry."""
-    _LOGGER.warning("FABER ITC: async_setup_entry starting for host: %s", entry.data.get(CONF_HOST))
+    _LOGGER.debug("Setting up integration for host: %s", entry.data.get(CONF_HOST))
     
-    # Registriert den Ordner 'branding' unter dem URL-Pfad '/faber_itc_static'
+    # Register 'branding' folder for entity icons
     await hass.http.async_register_static_paths(
         [
             StaticPathConfig(
@@ -25,21 +25,18 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     
     host = entry.data[CONF_HOST]
     client = FaberITCClient(host, DEFAULT_PORT)
-    
     coordinator = FaberITCUpdateCoordinator(hass, client)
     
-    # Initial load.
     await coordinator.async_config_entry_first_refresh()
     
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator
     
-    _LOGGER.warning("FABER ITC: Forwarding setup to platform: climate")
     await hass.config_entries.async_forward_entry_setups(entry, ["climate"])
-    _LOGGER.warning("FABER ITC: async_setup_entry finished successfully")
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(entry, ["climate"])
     if unload_ok:
         coordinator = hass.data[DOMAIN].pop(entry.entry_id)
